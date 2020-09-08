d538bc6c8c8b922fa4ea02bbd421406e0738e515
Yuem Park <yuempark@berkeley.edu>
2020-09-08 10:36:23 -0700

    implement pick/ban rates

--- league/src/01_data/feature_engineering.py
+++ league/src/01_data/feature_engineering.py
@@ -32,8 +32,6 @@ def BW_frac_plot(feature, unique_feature, unique_feature_BW_frac):
     ax.axhline(0.5, ls='--', c='k')
     ax.set_ylabel('blue win fraction')
     plt.show(fig)
-    
-    
 
 # how do patches correlate with wins?
 unique_patch, unique_patch_BW_frac = BW_frac_calc('patch')
@@ -46,4 +44,68 @@ unique_blue_pick1, unique_blue_pick1_BW_frac = BW_frac_calc('blue_pick1')
 BW_frac_plot('blue_pick1', unique_blue_pick1, unique_blue_pick1_BW_frac)
 
 # we see that there are outliers for champs that are likely only rarely picked
-# so create features that shows pick/ban rate
+# so create features that shows pick/ban rate per patch
+picks = ['1','2','3','4','5']
+for pick in picks:
+    prematch_data['blue_pick'+pick+'_rate'] = np.zeros(len(prematch_data))
+    prematch_data['blue_pick'+pick+'_ban_rate'] = np.zeros(len(prematch_data))
+    prematch_data['red_pick'+pick+'_rate'] = np.zeros(len(prematch_data))
+    prematch_data['red_pick'+pick+'_ban_rate'] = np.zeros(len(prematch_data))
+    for patch in unique_patch:
+        patch_slice = prematch_data[prematch_data['patch']==patch]
+        total_patch_games = len(patch_slice)
+        blue_pick_rate = np.zeros(total_patch_games)
+        blue_pick_ban_rate = np.zeros(total_patch_games)
+        red_pick_rate = np.zeros(total_patch_games)
+        red_pick_ban_rate = np.zeros(total_patch_games)
+        for i in range(total_patch_games):
+            blue_pick = patch_slice['blue_pick'+pick].iloc[i]
+            red_pick = patch_slice['red_pick'+pick].iloc[i]
+            blue_pick_rate[i] = np.sum(patch_slice['blue_pick'+pick]==blue_pick)/ \
+                                total_patch_games
+            blue_pick_ban_rate[i] = (np.sum(patch_slice['blue_pick'+pick]==blue_pick)+ \
+                                     np.sum(patch_slice['blue_ban1']==blue_pick)+ \
+                                     np.sum(patch_slice['blue_ban2']==blue_pick)+ \
+                                     np.sum(patch_slice['blue_ban3']==blue_pick)+ \
+                                     np.sum(patch_slice['blue_ban4']==blue_pick)+ \
+                                     np.sum(patch_slice['blue_ban5']==blue_pick)+ \
+                                     np.sum(patch_slice['red_ban1']==blue_pick)+ \
+                                     np.sum(patch_slice['red_ban2']==blue_pick)+ \
+                                     np.sum(patch_slice['red_ban3']==blue_pick)+ \
+                                     np.sum(patch_slice['red_ban4']==blue_pick)+ \
+                                     np.sum(patch_slice['red_ban5']==blue_pick))/ \
+                                    total_patch_games
+            red_pick_rate[i] = np.sum(patch_slice['red_pick'+pick]==red_pick)/ \
+                                      total_patch_games
+            red_pick_ban_rate[i] = (np.sum(patch_slice['red_pick'+pick]==red_pick)+ \
+                                    np.sum(patch_slice['blue_ban1']==red_pick)+ \
+                                    np.sum(patch_slice['blue_ban2']==red_pick)+ \
+                                    np.sum(patch_slice['blue_ban3']==red_pick)+ \
+                                    np.sum(patch_slice['blue_ban4']==red_pick)+ \
+                                    np.sum(patch_slice['blue_ban5']==red_pick)+ \
+                                    np.sum(patch_slice['red_ban1']==red_pick)+ \
+                                    np.sum(patch_slice['red_ban2']==red_pick)+ \
+                                    np.sum(patch_slice['red_ban3']==red_pick)+ \
+                                    np.sum(patch_slice['red_ban4']==red_pick)+ \
+                                    np.sum(patch_slice['red_ban5']==red_pick))/ \
+                                   total_patch_games
+        prematch_data.loc[prematch_data['patch']==patch, 'blue_pick'+pick+'_rate'] = blue_pick_rate
+        prematch_data.loc[prematch_data['patch']==patch, 'blue_pick'+pick+'_ban_rate'] = blue_pick_ban_rate
+        prematch_data.loc[prematch_data['patch']==patch, 'red_pick'+pick+'_rate'] = red_pick_rate
+        prematch_data.loc[prematch_data['patch']==patch, 'red_pick'+pick+'_ban_rate'] = red_pick_ban_rate
+    
+# distribution of pick/ban rates
+fig, ax = plt.subplots(nrows=5, ncols=2, figsize=(10,10), sharex=True, sharey=True)
+for i in range(len(picks)):
+    blue_win_slice = prematch_data[(prematch_data['blue_win']==1)]
+    blue_loss_slice = prematch_data[(prematch_data['blue_win']==0)]
+    bins = np.arange(0,1.05,0.05)
+    ax[i][0].hist(blue_win_slice['blue_pick'+picks[i]+'_ban_rate'], bins=bins, facecolor='C0', label='blue win', alpha=0.5)
+    ax[i][0].hist(blue_loss_slice['blue_pick'+picks[i]+'_ban_rate'], bins=bins, facecolor='C1', label='blue loss', alpha=0.5)
+    ax[i][0].set_ylabel('blue_pick'+picks[i]+'_ban_rate')
+    ax[i][1].hist(blue_win_slice['red_pick'+picks[i]+'_ban_rate'], bins=bins, facecolor='C0', label='blue win', alpha=0.5)
+    ax[i][1].hist(blue_loss_slice['red_pick'+picks[i]+'_ban_rate'], bins=bins, facecolor='C1', label='blue loss', alpha=0.5)
+    ax[i][1].set_ylabel('red_pick'+picks[i]+'_ban_rate')
+ax[0][0].set_xlim(0,1)
+ax[0][0].legend()
+plt.show(fig)
